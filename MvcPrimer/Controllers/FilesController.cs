58d150a54b1e3d8f399f702033270b26656e1181
Paolo Montano - Laptop <Paolo.Montano@Gmail.com>
2015-01-30 10:33:42 -0800

    First commit.

---
+++ MvcPrimer/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPrimer.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            DirectoryInfo dir = new DirectoryInfo(Server.MapPath("/files"));
+            List<string> files = new List<string>();
+            files = Directory.GetFiles(dir.ToString(), "*.txt").ToList();
+
+            return View(files);
+        }
+    }
+}
